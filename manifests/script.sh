85e6782f58f5cff6d944026120e513ed5b135633
prapoju <prapoju@gmail.com>
2026-05-01 20:19:51 -0500

    ci: create subdirectories and configure sonarqube in debug Jenkins file

--- manifests/script.sh
+++ manifests/script.sh
@@ -1,5 +1,8 @@
-# kind
+# kind: Create the cluster
 kind create cluster --config kind-jenkins-config.yaml
+
+# validate nodes creation
+kind get nodes -n ci-cd-demo
 
 
 # helm
@@ -8,29 +11,33 @@ helm repo update
 helm search repo jenkinsci
 
 # KUBECTL
-kubectl apply -f jenkins-namespace.yaml
+kubectl apply -f jenkins/jenkins-namespace.yaml
 
-kubectl apply -f jenkins-01-volume.yaml
+kubectl apply -f jenkins/jenkins-01-volume.yaml
 
 # Validate volume. THe volue exist, but the node affinity is none because the resource is not required by now. When we update the file, update permises.
 kubectl get pv -n jenkins
 
 
-kubectl apply -f jenkins-02-sa.yaml
+kubectl apply -f jenkins/jenkins-02-sa.yaml
 
 # Change nodeport, storageClass, service account false in the sa file
 # because the sa already exist
 
+# Define de chart
+# Create a release called jenkins in the name space jenkins that overrides the default conifg
+# using the jenkins-values.yaml and uses the chart
 
 chart=jenkinsci/jenkins
-helm install jenkins -n jenkins -f jenkins-values.yaml $chart
+helm install jenkins -n jenkins -f jenkins/jenkins-values.yaml $chart
 
 # Get passwod
 jsonpath="{.data.jenkins-admin-password}"
 secret=$(kubectl get secret -n jenkins jenkins -o jsonpath=$jsonpath)
 echo $(echo $secret | base64 --decode)
 
-# cE6m2sncAcj2M8q8YaUdp9
+# Example password
+# p3hks5SgdQOF78PBbW8uwg
 
 
 # But there is an error because of the permises
@@ -44,20 +51,58 @@ kubectl logs jenkins-0 -n jenkins -c init
 kubectl get pod jenkins-0 -n jenkins -o wide
 
 # Change permises
-docker exec jenkins-example-worker chown -R 1000:1000 /data/jenkins-volume
+docker exec ci-cd-demo-worker  chown -R 1000:1000 /data/jenkins-volume
 
 # Restart the pod
 kubectl delete pod jenkins-0 -n jenkins
 
-# Run this to do port forwarding
+# wait until the status is running
+kubectl get pods -n jenkins
+
+# get the password again if you can't sign in as admin
 kubectl -n jenkins port-forward jenkins-0 8080:8080
 
-# PLUGINS
+# sonarqube
 
-# Configuere the execution of agents dinamically AGENTS-KUBERNETES. The plugin was installed before
-# https://github.com/jenkinsci/kubernetes-plugin/blob/master/README.md
+kubectl get nodes
+kubectl get pods -n jenkins -o wide
 
-# Cloud kubernetes enable garbage collector. To avoid 
+
+# Choose a node name preferably not used by the jenkins pod. In my case it was ci-cd-demo-worker2
+#
+# we stop kubernetes from scheduling pods in the node
+kubectl taint nodes ci-cd-demo-worker2 sonarqube=true:NoSchedule --overwrite
+
+kubectl label node ci-cd-demo-worker2 sonarqube=true
+# Rules in the values.yaml
+# - sonarqube ignores the tail rule
+# - sonarqube uses nodes labeled as sonarqube
+# - monitoring passcode
+# - community edition enabled
+
+helm repo add sonarqube https://SonarSource.github.io/helm-chart-sonarqube
+
+helm repo update
+
+kubectl apply -f sonarqube/sonarqube-namespace.yaml
+
+helm upgrade --install \
+  -n sonarqube \
+  -f sonarqube/values.yaml \
+  sonarqube sonarqube/sonarqube
+
+# Wait for 5 minutes
+kubectl get pods -n sonarqube -o wide
+
+# Get key
+kubectl port-forward svc/sonarqube-sonarqube 9008:9000 -n sonarqube
+
+# Generate your token
+# Example: sqa_94a2eea9c7d8524f352be0aa97c2f6633b466c0e
+# Manage jenkins, system SONARQUBE SERVERS,
+# Server URL http://sonarqube-sonarqube.sonarqube:9000
+# Add secret  text paste secret scope global
+#
 
 
 
